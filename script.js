256dee70cad6a6ad694f096c4daa77d64b89d4e7
Neron <2022296soeung@aupp.edu.kh>
2023-09-30 10:45:20 +0700

    90

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = 80;
-    // Math.ceil(Math.random() * 10000);
 
-btn.onclick = function () {
-    container.style.transform = "rotate(" + number + "deg)";
-    number += Math.ceil(Math.random() * 100000) + 360-number;
-}
-console.log(number)
+let number = 160 + Math.floor(Math.random() * 129600)*360;
+let max = 720;
+console.log("1", number);
+
+btn.addEventListener("click", () => {
+    container.style.transform = `rotate(${number}deg)`;
+
+    number += (Math.floor(Math.random() * 129600))*360 + 360 - number;
+    console.log("2", number);
+
+    if(number >= max) {
+        number =  (Math.floor(Math.random() * 129600))*360+  360 - number;
+        console.log("3", number);
+
+    }
+});
