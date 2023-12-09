8765dac5783c44001a1c0aaf6a77de2c5cfeac09
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2023-12-09 13:59:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,94 +15,26 @@ let tokens = [
 ];
 let currentIndex = 0;
 
-// Function to shuffle the tokens array randomly
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+function sendRequest() {
+  fetch('https://grouping.onrender.com/exclude', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Basic ' + btoa('guest:1234') // Base64 encode username:password
     }
-}
-btn.addEventListener("click", () => {
-    if (currentIndex === 0) {
-        shuffleArray(tokens); // Shuffle tokens when starting a new rotation
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-
-    if (currentIndex < tokens.length) {
-        let token = tokens[currentIndex];
-        let number = token + Math.floor(Math.random() * 129600) * 360 + 360 + token;
-        let max = 720;
-
-        container.style.transform = `rotate(${number}deg)`;
-
-        number += Math.floor(Math.random() * 129600) * 360 + 360 + token;
-
-        if (number >= max) {
-            number = Math.floor(Math.random() * 129600) * 360 + 360 + token;
-        }
-
-        currentIndex++; // Move to the next token
-
-        if (currentIndex === tokens.length && tokens[0] === 200 && tokens[0] === 19&& tokens[0] === 17 && token !== 570) {
-            currentIndex = 0; // Reset currentIndex after all tokens have been rotated
-        }
-        if (token !== 200 && token !== 19 && token !== 17 && token !== 570) {
-            btn.remove(); // Reset currentIndex after all tokens have been rotated
-
-        }
-     
-
-    }
-});
-// Define the base URL of your web application
-const baseURL = 'https://grouping.onrender.com';
-
-// Define the authentication credentials
-const credentials = {
-    user: { username: 'USERNAME', password: 'PASSWORD' },
-    guest: { username: 'GUEST_USERNAME', password: 'GUEST_PASSWORD' },
-    dev: { username: 'DEV_USERNAME', password: 'DEV_PASSWORD' },
-};
-
-// Function to send requests continuously
-async function sendContinuousRequests() {
-    try {
-        // Adjust the endpoints and authentication details based on your use case
-
-        // Example: Send a request to the homepage
-        const response1 = await fetch(`${baseURL}/`);
-        const data1 = await response1.json(); // Assuming the response is JSON, adjust accordingly
-        console.log('Response 1:', response1.status, data1);
-
-        // Example: Send a request to the list endpoint
-        const response2 = await fetch(`${baseURL}/list`, {
-            headers: {
-                Authorization: `Basic ${btoa(`${credentials.user.username}:${credentials.user.password}`)}`,
-            },
-        });
-        const data2 = await response2.json(); // Assuming the response is JSON, adjust accordingly
-        console.log('Response 2:', response2.status, data2);
-
-        // Example: Send a request to the addlist endpoint
-        const response3 = await fetch(`${baseURL}/addlist`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Basic ${btoa(`${credentials.dev.username}:${credentials.dev.password}`)}`,
-            },
-            body: JSON.stringify([{ name: 'John Doe' }]),
-        });
-        const data3 = await response3.json(); // Assuming the response is JSON, adjust accordingly
-        console.log('Response 3:', response3.status, data3);
-
-        // Add more requests as needed
-
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-
-    // Repeat the function call after a delay (adjust as needed)
-    setTimeout(sendContinuousRequests, 1000);
+    return response.json();
+  })
+  .then(data => {
+    console.log('Response:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 }
 
-// Start sending requests continuously
-sendContinuousRequests();
+// Set up interval to send the request every 10 seconds (10000 milliseconds)
+setInterval(sendRequest, 10000);
